2e073cfe5c614d8bc9508550c455b4dfc3b4b4e7
Miguel Martinez <miguel@bitnami.com>
2017-05-28 16:17:44 -0700

    Logging

--- repo-sync.sh
+++ repo-sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,6 +10,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+log () {
+  echo -e "\033[0;33m$(date "+%H:%M:%S")\033[0;37m ==> $1."
+}
 
 travis_setup_git() {
   git config user.email "travis@travis-ci.org"
@@ -36,13 +41,17 @@ show_important_vars
 # Package all charts and update index in temporary buildDir
 pushd $BUILD_DIR
   for dir in `ls $REPO_DIR/charts`;do
+    log "Packaging $dir"
     helm dep update $REPO_DIR/charts/$dir
     helm package $REPO_DIR/charts/$dir
   done
+
+  log "Indexing repository"
   helm repo index --url ${REPO_URL} .
 popd
 
-if [! -z $TRAVIS ]; then
+if [ ! -z $TRAVIS ]; then
+  log "Configuring git for Travis-ci"
   travis_setup_git
 else
   git remote add upstream git@github.com:migmartri/helm-hack-night-charts.git || true
@@ -52,6 +61,7 @@ git fetch upstream
 git reset upstream/gh-pages
 cp $BUILD_DIR/* $REPO_DIR
 
-#git add -A $REPO_DIR
-#git commit --message "$COMMIT_MSG"
-#git push upstream HEAD:gh-pages
+log "Commiting changes to gh-pages branch"
+git add *.tgz index.yaml
+git commit --message "$COMMIT_MSG"
+git push upstream HEAD:gh-pages
